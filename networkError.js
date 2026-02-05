30d9e6af1b977ea5d857dd9a1c180db503594f5e
scriptMang <aperalta686@gmail.com>
2026-02-04 22:16:31 -0500

    Add func validate-network-conn to network-error class

--- networkError.js
+++ networkError.js
@@ -1,8 +1,14 @@
+
 class NetworkError extends Error {
     constructor(message) {
         super(message);
         this.name = "NetworkError";
     }
 
+    validateNetworkConn() {
+        if (Math.Floor((Math.Random()*8)+1 > 5)){
+            throw new NetworkError("Failed to connect to url.");
+        }
+    }
 
 }
